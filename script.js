9044ce543b73284f81249a7195efaa91b8858edd
Tristen <protonvert@gmail.com>
2023-02-08 22:07:13 -0600

    Upgrade functionality and styling This is the essentially the completed project for a local version. I will be revising this once I add server functionality. The styling and functionality were upgraded to their final stage.

---
+++ script.js
@@ -0,0 +1,155 @@
+/* eslint-disable no-plusplus */
+/* eslint-disable no-console */
+/* eslint-disable prefer-destructuring */
+/* eslint-disable no-unused-vars */
+/* eslint-disable prefer-const */
+const addBookButton = document.querySelector('#add-book');
+
+const bookGrid = document.querySelector('.book--grid');
+const formPopUp = document.querySelector('.form-popup');
+
+const submitBook = document.querySelector('#submit-new-book');
+
+const exitForm = document.querySelector('img[src="icons/exit.svg"]');
+
+let discardCard;
+// = document.querySelectorAll('.book--card-remove');
+
+let bookCardToggleRead;
+
+let bookCards;
+
+const myLibrary = []; // array of book objects
+
+function Book() {
+  // book object constructor=
+}
+
+Book.prototype = {
+  title: '',
+  author: '',
+  pages: undefined,
+  read: 'false',
+
+  setTitle(title) {
+    this.title = title;
+  },
+  setAuthor(author) {
+    this.author = author;
+  },
+  setPages(pages) {
+    this.pages = pages;
+  },
+  setRead(readStatus) {
+    this.read = readStatus;
+  },
+  updateReadStatus() {
+    if (this.read === 'Read ✅') {
+      this.setRead('Not Read ❌');
+    } else if (this.read === 'Not Read ❌') {
+      this.setRead('Read ✅');
+    }
+  },
+  addCardToPage() {
+    let bookCard = document.createElement('div');
+    bookCard.setAttribute('class', `book--card ${this.title}`);
+    let bookTitle = document.createElement('h1');
+    bookTitle.setAttribute('class', 'book--card-bookTitle');
+    let removeBook = document.createElement('img');
+    removeBook.setAttribute('class', 'book--card-remove');
+    removeBook.src = 'icons/exit.svg';
+
+    let bookCardAuthor = document.createElement('h2');
+    bookCardAuthor.setAttribute('class', 'book--card-author');
+
+    let bookCardInfoSection = document.createElement('div');
+    bookCardInfoSection.setAttribute('class', 'book--card-infosection');
+    let bookCardPages = document.createElement('span');
+    bookCardPages.setAttribute('class', 'book--card-pages');
+    let bookCardRead = document.createElement('span');
+    bookCardRead.setAttribute('class', 'book--card-read');
+    bookCardInfoSection.appendChild(bookCardPages);
+    bookCardInfoSection.appendChild(bookCardRead);
+    bookCardInfoSection.setAttribute('class', 'book--card-infosection');
+    let toggleRead = document.createElement('button');
+    toggleRead.setAttribute('class', 'book--card-toggleRead');
+
+    bookCard.appendChild(removeBook);
+    bookCard.appendChild(bookTitle);
+    bookCard.appendChild(bookCardAuthor);
+    bookCard.appendChild(bookCardInfoSection);
+    bookCard.appendChild(toggleRead);
+
+    toggleRead.textContent = 'Update Read Status';
+    bookTitle.textContent = this.title;
+    bookCardAuthor.textContent = this.author;
+    bookCardPages.textContent = `Pages: ${this.pages}`;
+    bookCardRead.textContent = this.read;
+
+    bookGrid.appendChild(bookCard);
+  },
+};
+
+addBookButton.addEventListener('click', () => {
+  formPopUp.style.display = 'block';
+});
+
+function getValues() {
+  let valueArray = Array.from(
+    document.querySelectorAll('.form-container input'),
+  );
+
+  let attributeValues = valueArray.map((input) => `${input.value}`);
+
+  let newBook = new Book();
+
+  newBook.setTitle(attributeValues[0]);
+  newBook.setAuthor(attributeValues[1]);
+  newBook.setPages(attributeValues[2]);
+  if (document.getElementById('not-read').checked) {
+    newBook.setRead('Not Read ❌');
+  } else if (document.getElementById('read').checked) {
+    newBook.setRead('Read ✅');
+  }
+  myLibrary.push(newBook);
+}
+
+function refreshCards() {
+  bookGrid.innerHTML = '';
+  myLibrary.forEach((bookElement) => {
+    bookElement.addCardToPage();
+  });
+  bookCardToggleRead = document.querySelectorAll('.book--card-toggleRead');
+  discardCard = document.querySelectorAll('.book--card-remove');
+
+  for (let i = 0; i < bookCardToggleRead.length; i++) {
+    bookCardToggleRead[i].addEventListener('click', () => {
+      myLibrary[i].updateReadStatus();
+      refreshCards();
+    });
+  }
+  for (let i = 0; i < discardCard.length; i++) {
+    discardCard[i].addEventListener('click', () => {
+      myLibrary.remove(i);
+      refreshCards();
+    });
+  }
+}
+
+myLibrary.remove = (index) => {
+  myLibrary.splice(index, 1);
+  refreshCards();
+};
+
+submitBook.addEventListener('click', () => {
+  event.preventDefault();
+  formPopUp.style.display = 'none';
+  getValues();
+  document.querySelector('.form-container').reset();
+  refreshCards();
+});
+
+exitForm.addEventListener('click', () => {
+  document.querySelector('.form-container').reset();
+  formPopUp.style.display = 'none';
+});
